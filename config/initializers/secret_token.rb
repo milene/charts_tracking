7cec6e0b33de26bd0303e0eb595ceea948400ac9
milene.darnis@gmail.com <milene@Milenes-MacBook-Air.local>
2013-05-31 17:49:47 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChartsDeployed::Application.config.secret_token = '57d84211af2f587efbbf33211461b9a67b05b82accbf771f5f758487c28ba2284fe8e03912ea6518bfec43180b8b4e325ae27922e73ce4f4aa59bcb6ebe23d95'
